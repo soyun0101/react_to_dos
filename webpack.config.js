b3bbc88c1bd26ab5aafc4f11e0afe9e9e34996a1
Sophia <sophiayun@yahoo.com>
2020-01-25 11:39:03 -0800

    added webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+const path= require('path');
+
+module.exports={
+    resolve:{
+        extensions: ['.js', '.jsx']
+    },
+    module:{
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: {
+                    loader: 'babel-loader',
+                    options:{
+                        plugins:[
+                            '@babel/plugin-transform-react-jsx'
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+        noInfo: true
+    }
+}
